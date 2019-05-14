55904ae207816fde9cc2187a57a303992e4778dc
AT4-CGSG <A.Tsutsiev@yandex.ru>
2019-05-14 21:21:57 +0300

    Update BottleSong.java
    You can do it by clonig

--- P3123_Tsutsiev_Andrey_task-1/BottleSong.java
+++ P3123_Tsutsiev_Andrey_task-1/BottleSong.java
@@ -21,19 +21,22 @@ public class BottleSong
 
     private void makeSong( int bottleTakenAtOnce ) {
         int bottlesLeft = 99;
+        StringBuilder TheSongBuilder = new StringBuilder();
 
         if (!TheSong.isEmpty())
             TheSong = "";
 
         while (bottlesLeft >= bottleTakenAtOnce)
-            TheSong += sBottlesLeft(bottlesLeft) + " of beer on the wall, " + sBottlesLeft(bottlesLeft) + " of beer.\n" +
-                    "Take " + sBottlesTake(bottleTakenAtOnce) + " down and pass around, " + sBottlesLeft(bottlesLeft -= bottleTakenAtOnce) + " of beer on the wall.\n";
+            TheSongBuilder.append(String.format("%s of beer on the wall, %s of beer.\nTake %s down and pass around, %s of beer on the wall.\n",
+            sBottlesLeft(bottlesLeft), sBottlesLeft(bottlesLeft), sBottlesTake(bottleTakenAtOnce), sBottlesLeft(bottlesLeft -= bottleTakenAtOnce)));
 
         if (bottlesLeft > 0)
-            TheSong += sBottlesLeft(bottlesLeft) + " of beer on the wall, " + sBottlesLeft(bottlesLeft) + " of beer.\n" +
-                    "Take " + sBottlesTake(bottlesLeft) + " down and pass around, "+ sBottlesLeft(0) + " of beer on the wall.\n";
+            TheSongBuilder.append(String.format("%s of beer on the wall, %s of beer.\nTake %s down and pass around, %s of beer on the wall.\n",
+                    sBottlesLeft(bottlesLeft), sBottlesLeft(bottlesLeft), sBottlesTake(bottlesLeft), sBottlesLeft(0)));
 
-        TheSong += "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n";
+        TheSongBuilder.append("No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n");
+
+        TheSong = TheSongBuilder.toString();
     }
 
     private String sBottlesLeft( int amount ) { return ((amount > 0) ? (new Integer(amount)).toString() : "no more") + (amount == 1 ? " bottle" : " bottles"); }
