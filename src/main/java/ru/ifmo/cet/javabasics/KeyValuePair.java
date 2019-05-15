75199f9fd87d9460d48386747ef1aea05c11c65f
ilyaryabikin <ilyaryabikin0708@gmail.com>
2019-05-16 01:55:48 +0300

    Done

---
+++ src/main/java/ru/ifmo/cet/javabasics/KeyValuePair.java
@@ -0,0 +1,31 @@
+package ru.ifmo.cet.javabasics;
+
+class KeyValuePair implements Comparable<KeyValuePair>{
+    private String key;
+    private int value;
+
+    KeyValuePair(int value, String key) {
+        super();
+        this.key = key;
+        this.value = value;
+    }
+
+    int getValue() {
+        return value;
+    }
+
+    String getKey() {
+        return key;
+    }
+
+    public int compareTo(KeyValuePair other) {
+        if(value != other.value)
+        {
+            return other.value - value;
+        }
+        else
+        {
+            return key.compareTo(other.key);
+        }
+    }
+}
--- src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
+++ src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
@@ -1,21 +1,53 @@
 package ru.ifmo.cet.javabasics;
 
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.util.*;
 
 
 public class WarAndPeaceExercise {
+
+    private static String readFileAsString(Path path) throws IOException {
+        return new String(Files.readAllBytes(path));
+    }
 
     public static String warAndPeace() {
         final Path tome12Path = Paths.get("src", "main", "resources", "WAP12.txt");
         final Path tome34Path = Paths.get("src", "main", "resources", "WAP34.txt");
 
-        // TODO map lowercased words to its amount in text and concatenate its entries.
-        // TODO If word "котик" occurred in text 23 times then its entry would be "котик - 23\n".
-        // TODO Entries in final String should be also sorted by amount and then in alphabetical order if needed.
-        // TODO Also omit any word with lengths less than 4 and frequency less than 10
+        try {
+            String fullText = readFileAsString(tome12Path) + readFileAsString(tome34Path);
+            String[] wordsArray = fullText.toLowerCase().split("(?U)[\\W\\d]");
 
-        throw new UnsupportedOperationException();
+            Map<String, Integer> wordsMap = new HashMap<>();
+
+            for (String word : wordsArray) {
+                if (word.length() >= 4) {
+                    wordsMap.put(word, wordsMap.getOrDefault(word, 0) + 1);
+                }
+            }
+
+            StringBuilder result = new StringBuilder();
+            List<Map.Entry<String, Integer>> list = new ArrayList<>(wordsMap.entrySet());
+            SortedSet<KeyValuePair> sortedSet = new TreeSet<>();
+
+            for(Map.Entry<String, Integer> item : list)
+            {
+                sortedSet.add(new KeyValuePair(item.getValue(),item.getKey()));
+            }
+
+            for (KeyValuePair p : sortedSet) {
+                if (p.getValue() >= 10) {
+                    result.append(p.getKey()).append(" - ").append(p.getValue()).append("\n");
+                }
+            }
+
+            return result.deleteCharAt(result.length()-1).toString();
+        }
+        catch (IOException e) {
+            return e.getMessage();
+        }
     }
-
 }
