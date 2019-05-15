9e11637db17587c3b1afe7b2161e8c07464dd924
InD1ggo <usr.tt@yandex.ru>
2019-05-15 19:05:27 +0300

    task-1 (fix fix)

--- src/main/java/ru/ifmo/cet/javabasics/BottleSong.java
+++ src/main/java/ru/ifmo/cet/javabasics/BottleSong.java
@@ -30,50 +30,58 @@ package ru.ifmo.cet.javabasics;
  * Значение передается в качестве параметра конструктора
  * Нужно ограничить возможность взятия бутылок натуральным число не более 99 бутылок за раз.
  */
-public class BottleSong {
+public class BottleSong
+    {
 
-    private String[] tens = {"", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+    private static String[] tens = {"", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
 
-    private String[] upToTwenty = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+    private static String[] Twenty = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
 
-    private int Taken;
+    private String toWords(int num)
+       {
+        if (num <= 19)
+            return Twenty[num];
+        else if (num % 10 == 0)
+            return tens[num / 10];
+        else
+            return tens[num / 10] + " " + Twenty[num % 10];
+       }
+
+       private int Taken;
 
     public BottleSong(int bottleTakenAtOnce)
-    {
-        this.Taken = bottleTakenAtOnce;
-    }
+       {
+        Taken = bottleTakenAtOnce;
+       }
 
-    private String inWord(int bottleTakenAtOnce)
-    {
-        if (bottleTakenAtOnce <= 19)
-            return upToTwenty[bottleTakenAtOnce];
-        else if (bottleTakenAtOnce % 10 == 0)
-            return tens[bottleTakenAtOnce / 10];
-        else
-            return tens[bottleTakenAtOnce / 10] + " " + upToTwenty[bottleTakenAtOnce % 10];
-    }
 
     public String getBottleSongLyrics()
+
     {
         if (Taken <= 0 || Taken > 99)
-        {
+           {
             throw new IllegalArgumentException();
-        }
+           }
+
 
         StringBuilder result = new StringBuilder();
+
+
         int i;
-
-
         for (i = 99; i > Taken; i -= Taken)
-        {
+           {
             result.append(i + " bottles of beer on the wall, " + i + " bottles of beer.\n");
-            result.append("Take " + inWord(Taken) + " down and pass around, " + (i - Taken) + (i - Taken == 1 ? " bottle" : " bottles") + " of beer on the wall.\n");
-        }
+            result.append("Take " + toWords(Taken) + " down and pass around, " + (i - Taken) + (i - Taken == 1 ? " bottle" : " bottles") + " of beer on the wall.\n");
+           }
 
-        result.append(i + (i == 1 ? " bottle" : " bottles") + " of beer on the wall, " + i + (i == 1 ? " bottle" : " bottles") + " of beer.\n" +
-                "Take " + inWord(i) + " down and pass around, no more bottles of beer on the wall.\n" +
-                "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n");
+        result.append(i + (i == 1 ? " bottle" : " bottles") + " of beer on the wall, " + i + (i == 1 ? " bottle" : " bottles") + " of beer.\n");
+        result.append("Take " + toWords(i) + " down and pass around, no more bottles of beer on the wall.\n");
+
+
+        result.append("No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n");
+
 
         return result.toString();
     }
+
 }
