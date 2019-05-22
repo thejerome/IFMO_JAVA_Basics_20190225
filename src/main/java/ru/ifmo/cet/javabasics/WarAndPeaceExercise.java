55a54f5358f0805ccf96fe360d1f3846c687bc65
ilyaryabikin <ilyaryabikin0708@gmail.com>
2019-05-22 22:40:55 +0300

    Changed smth just ff

--- src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
+++ src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
@@ -9,11 +9,44 @@ import java.util.*;
 
 public class WarAndPeaceExercise {
 
-    private static String[] readFileAsStrings(Path path) throws IOException {
-        List<String> fileSet = Files.readAllLines(path, Charset.forName("windows-1251"));
-        return Arrays.toString(fileSet.toArray())
-                .toLowerCase()
-                .split("([^А-я\\w]|[\\s\\d])+");
+    static class KeyValuePair implements Comparable<KeyValuePair>{
+        private String key;
+        private int value;
+
+        KeyValuePair(int value, String key) {
+            super();
+            this.key = key;
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public String getKey() {
+            return key;
+        }
+
+        @Override
+        public int compareTo(KeyValuePair other) {
+            if (value != other.value) {
+                return other.value - value;
+            }
+            else {
+                return key.compareTo(other.key);
+            }
+        }
+    }
+
+    private static String[] readFileAsStrings(Path path) {
+        try {
+            List<String> fileSet = Files.readAllLines(path, Charset.forName("windows-1251"));
+            return Arrays.toString(fileSet.toArray())
+                    .toLowerCase()
+                    .split("([^а-я\\w]|[\\s\\d])+");
+        } catch (IOException e) {
+            return new String[0];
+        }
     }
 
     private static void putWordsToMap(Map<String, Integer> map, String[] array) {
@@ -28,33 +61,27 @@ public class WarAndPeaceExercise {
         final Path tome12Path = Paths.get("src", "main", "resources", "WAP12.txt");
         final Path tome34Path = Paths.get("src", "main", "resources", "WAP34.txt");
 
-        try {
-            String[] tome12String = readFileAsStrings(tome12Path);
-            String[] tome34String = readFileAsStrings(tome34Path);
+        String[] tome12String = readFileAsStrings(tome12Path);
+        String[] tome34String = readFileAsStrings(tome34Path);
 
-            Map<String, Integer> wordsMap = new HashMap<>();
+        Map<String, Integer> wordsMap = new HashMap<>();
 
-            putWordsToMap(wordsMap, tome12String);
-            putWordsToMap(wordsMap, tome34String);
+        putWordsToMap(wordsMap, tome12String);
+        putWordsToMap(wordsMap, tome34String);
 
-            SortedSet<KeyValuePair> sortedMapSet = new TreeSet<>();
+        SortedSet<KeyValuePair> sortedMapSet = new TreeSet<>();
 
-            for(Map.Entry<String, Integer> item : wordsMap.entrySet())
-            {
-                sortedMapSet.add(new KeyValuePair(item.getValue(),item.getKey()));
-            }
-
-            StringBuilder result = new StringBuilder();
-            for (KeyValuePair p : sortedMapSet) {
-                if (p.getValue() >= 10) {
-                    result.append(p.getKey()).append(" - ").append(p.getValue()).append("\n");
-                }
-            }
-
-            return result.deleteCharAt(result.length()-1).toString();
+        for (Map.Entry<String, Integer> item : wordsMap.entrySet()) {
+            sortedMapSet.add(new KeyValuePair(item.getValue(), item.getKey()));
         }
-        catch (IOException e) {
-            return e.getMessage();
+
+        StringBuilder result = new StringBuilder();
+        for (KeyValuePair p : sortedMapSet) {
+            if (p.getValue() >= 10) {
+                result.append(p.getKey()).append(" - ").append(p.getValue()).append("\n");
+            }
         }
+
+        return result.deleteCharAt(result.length() - 1).toString();
     }
 }
