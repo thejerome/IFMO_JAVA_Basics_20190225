e2ce1e090a452bf4375ccb4f9a82bd92fc9f2a7d
ekaterinadidenko20 <ekaterinadidenko20@gmail.com>
2019-05-22 22:14:06 +0300

    qwer

--- src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
+++ src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
@@ -1,21 +1,60 @@
 package ru.ifmo.cet.javabasics;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.*;
 
 
 public class WarAndPeaceExercise {
 
-    public static String warAndPeace() {
+    public static String warAndPeace() throws IOException {
         final Path tome12Path = Paths.get("src", "main", "resources", "WAP12.txt");
         final Path tome34Path = Paths.get("src", "main", "resources", "WAP34.txt");
 
-        // TODO map lowercased words to its amount in text and concatenate its entries.
-        // TODO If word "котик" occurred in text 23 times then its entry would be "котик - 23\n".
-        // TODO Entries in final String should be also sorted by amount and then in alphabetical order if needed.
-        // TODO Also omit any word with lengths less than 4 and frequency less than 10
+        List<String> tom12 = Files.readAllLines(tome12Path, Charset.forName("windows-1251"));
+        List<String> tom34 = Files.readAllLines(tome34Path, Charset.forName("windows-1251"));
+        ArrayList<String> lines = new ArrayList<>();
+        lines.addAll(tom12);
+        lines.addAll(tom34);
 
-        throw new UnsupportedOperationException();
+        Map<String, Integer> map = new TreeMap();
+
+        for(String cur : lines){
+            String llines = cur.toLowerCase().replaceAll("[^а-яa-z]", " ");
+            for(String word : llines.split(" ")){
+                if(word.length()>=4){
+                    if(map.containsKey(word))
+                        map.put(word,map.get(word)+1);
+                    else
+                        map.put(word,1);
+                }
+            }
+        }
+
+        return wordsSort(map);
+    }
+
+    public static String wordsSort(Map<String,Integer> map){
+
+        List< Map.Entry<String,Integer> > ws = new ArrayList<>(map.entrySet());
+
+        Collections.sort(ws, new Comparator< Map.Entry<String,Integer> >(){
+            public int compare(Map.Entry<String,Integer> a, Map.Entry<String,Integer> b){
+                if(a.getValue().equals(b.getValue())) return a.getKey().compareTo(b.getKey());
+                else return -(a.getValue()).compareTo(b.getValue());
+            }
+        });
+
+        String res = "";
+
+        for(Map.Entry<String,Integer> kk : ws){
+            if(kk.getValue()>=10) res += kk.getKey()+" - "+kk.getValue()+"\n";
+        }
+
+        return res.trim();
     }
 
 }
