874ccbd8a7b90c219a195e9cf470bf2c6e53a888
xe-esina <265105@niuitmo.ru>
2019-05-22 23:12:57 +0300

    done task-2

--- src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
+++ src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
@@ -1,21 +1,47 @@
 package ru.ifmo.cet.javabasics;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-
+import java.nio.file.Files;
+import java.util.*;
 
 public class WarAndPeaceExercise {
 
-    public static String warAndPeace() {
+    public static String warAndPeace() throws IOException {
         final Path tome12Path = Paths.get("src", "main", "resources", "WAP12.txt");
         final Path tome34Path = Paths.get("src", "main", "resources", "WAP34.txt");
 
-        // TODO map lowercased words to its amount in text and concatenate its entries.
-        // TODO If word "котик" occurred in text 23 times then its entry would be "котик - 23\n".
-        // TODO Entries in final String should be also sorted by amount and then in alphabetical order if needed.
-        // TODO Also omit any word with lengths less than 4 and frequency less than 10
+        List<String> lines = Files.readAllLines(tome12Path, Charset.forName("windows-1251"));
+        lines.addAll(Files.readAllLines(tome34Path, Charset.forName("windows-1251")));
 
-        throw new UnsupportedOperationException();
+        String[] words = Arrays.toString(lines.toArray()).toLowerCase().split("[^a-zа-я]+");
+
+        Map<String, Integer> wordsMap = new HashMap<>();
+
+        for (String word : words)
+            if (word.length() >= 4)
+                wordsMap.put(word, wordsMap.getOrDefault(word, 0) + 1);
+
+        SortedSet<Map.Entry<String, Integer>> answers = new TreeSet<>(
+                new Comparator<Map.Entry<String, Integer>>() {
+                    @Override
+                    public int compare(Map.Entry<String, Integer> a, Map.Entry<String, Integer> b) {
+                        if (!a.getValue().equals(b.getValue()))
+                            return b.getValue() - a.getValue();
+                        else
+                            return a.getKey().compareTo(b.getKey());
+                    }
+                }
+        );
+        answers.addAll(wordsMap.entrySet());
+
+        StringBuilder result = new StringBuilder();
+        for (Map.Entry<String, Integer> entry : answers)
+            if (entry.getValue() >= 10)
+                result.append(entry.getKey()).append(" - ").append(entry.getValue()).append("\n");
+
+        return result.deleteCharAt(result.length()-1).toString();
     }
-
 }
