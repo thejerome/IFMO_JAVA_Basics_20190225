739b76de7a1b03984fd007e201fdf39e88c08685
Vikatimoshenko3103 <lala3103@mail.ru>
2019-05-22 21:52:48 +0300

    Done

--- src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
+++ src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
@@ -1,21 +1,39 @@
 package ru.ifmo.cet.javabasics;
 
-import java.nio.file.Path;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.*;
+import java.nio.file.Files;
 import java.nio.file.Paths;
-
+import java.util.*;
+import java.util.function.Predicate;
 
 public class WarAndPeaceExercise {
 
-    public static String warAndPeace() {
+    public static String warAndPeace() throws IOException {
+
         final Path tome12Path = Paths.get("src", "main", "resources", "WAP12.txt");
         final Path tome34Path = Paths.get("src", "main", "resources", "WAP34.txt");
+        List<String> listNovel = Files.readAllLines(tome12Path, Charset.forName("windows-1251"));
+        listNovel.addAll(Files.readAllLines(tome34Path, Charset.forName("windows-1251")));
 
-        // TODO map lowercased words to its amount in text and concatenate its entries.
-        // TODO If word "котик" occurred in text 23 times then its entry would be "котик - 23\n".
-        // TODO Entries in final String should be also sorted by amount and then in alphabetical order if needed.
-        // TODO Also omit any word with lengths less than 4 and frequency less than 10
+        Map<String, Integer> words = new HashMap<>();
+        for (String Line : listNovel)
+            for (String word : Line.split("[\\s,.[0-9]«»()?!\\]\\[;:'\\“\"…„]+"))
+                if ((word = word.toLowerCase()).length() >= 4)
+                    words.put(word, 1 + (words.keySet().contains(word) ? words.get(word) : 0));
 
-        throw new UnsupportedOperationException();
+        List<Map.Entry<String, Integer>> wordFreq = new ArrayList<>(words.entrySet());
+        wordFreq.sort(Map.Entry.comparingByKey());
+        wordFreq.sort(Map.Entry.comparingByValue(new Comparator<Integer>() {
+            @Override public int compare(Integer x, Integer y) { return y - x; }}));
+        wordFreq.removeIf(new Predicate<Map.Entry<String, Integer>>() {
+            @Override public boolean test(Map.Entry<String, Integer> x) { return x.getValue() < 10; }});
+
+        StringBuilder result = new StringBuilder();
+        for (Map.Entry<String, Integer> p : wordFreq)
+            result.append(p.getKey() + " - " + p.getValue() + "\n");
+
+        return result.substring(0, result.length() - 1);
     }
-
 }
