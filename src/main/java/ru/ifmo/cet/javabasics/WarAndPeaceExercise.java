74479c9c44a190f147450146552332ee5ad0f141
aumetov7 <aumetov7@gmail.com>
2019-05-25 23:08:44 +0300

    commit

--- src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
+++ src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
@@ -1,21 +1,86 @@
 package ru.ifmo.cet.javabasics;
 
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.*;
 
 
 public class WarAndPeaceExercise {
 
-    public static String warAndPeace() {
+    private static String textSplitted = "";
+    private static String text;
+    private static int i;
+    private static int count;
+    private static Map<String, Integer> alphabetMap;
+    private static String res;
+    private static String[] arrText;
+    private static String result;
+
+
+    public static String warAndPeace() throws IOException {
         final Path tome12Path = Paths.get("src", "main", "resources", "WAP12.txt");
         final Path tome34Path = Paths.get("src", "main", "resources", "WAP34.txt");
+        List<String> firstFile = Files.readAllLines(tome12Path, Charset.forName("windows-1251"));
+        List<String> secondFile = Files.readAllLines(tome34Path, Charset.forName("windows-1251"));
+        firstFile.addAll(secondFile);
+        text = "";
+        count = 0;
+        alphabetMap = new TreeMap<>();
+        res = "";
+        textSplitted = "";
 
-        // TODO map lowercased words to its amount in text and concatenate its entries.
-        // TODO If word "котик" occurred in text 23 times then its entry would be "котик - 23\n".
-        // TODO Entries in final String should be also sorted by amount and then in alphabetical order if needed.
-        // TODO Also omit any word with lengths less than 4 and frequency less than 10
 
-        throw new UnsupportedOperationException();
+        for (String s : firstFile) {
+            text = s.toLowerCase().replaceAll("[^a-zа-я]", " ");
+            for (String splited : text.split(" ")) {
+                if (3 < splited.length()) {
+                    if (alphabetMap.containsKey(splited)) {
+                        alphabetMap.put(splited, alphabetMap.get(splited) + 1);
+                    } else {
+                        alphabetMap.put(splited, 1);
+                    }
+                }
+            }
+        }
+
+        for (String re : alphabetMap.toString().trim().replaceAll("[{}1234567890,=]", "").split(" ")) {
+            if (alphabetMap.get(re) < 10) {
+                alphabetMap.remove(re);
+            }
+        }
+        alphabetMap = sortByValue(alphabetMap);
+        res = alphabetMap.toString().trim().replaceAll("[{}]", "");
+        res = res.replace("=", " - ");
+        result = "";
+        for (String slova : res.trim().split(", ")) {
+            result += slova + "\n";
+        }
+        result = result.trim();
+
+        return result;
+    }
+
+
+    public static <K, V extends Comparable<? super V>> Map<K, V>
+    sortByValue(Map<K, V> map) {
+        List<Map.Entry<K, V>> list =
+                new LinkedList<>(map.entrySet());
+        Collections.sort(list, new Comparator<Map.Entry<K, V>>() {
+            @Override
+            public int compare(Map.Entry<K, V> o1, Map.Entry<K, V> o2) {
+                return (o2.getValue()).compareTo(o1.getValue());
+            }
+
+        });
+
+        Map<K, V> result = new LinkedHashMap<>();
+        for (Map.Entry<K, V> entry : list) {
+            result.put(entry.getKey(), entry.getValue());
+        }
+        return result;
     }
 
 }
