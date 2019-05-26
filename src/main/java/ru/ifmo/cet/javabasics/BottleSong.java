8e1d5d0f578830e443b872e19d247e05601db540
Vrezh <vrezhdevelop@mail.ru>
2019-05-26 20:52:09 +0300

    Solved

--- src/main/java/ru/ifmo/cet/javabasics/BottleSong.java
+++ src/main/java/ru/ifmo/cet/javabasics/BottleSong.java
@@ -1,7 +1,7 @@
 package ru.ifmo.cet.javabasics;
 
 /**
- * Нужно реализовать констурктор и метод, возвращающий слова песни про бутылки на стене.
+     * Нужно реализовать констурктор и метод, возвращающий слова песни про бутылки на стене.
  * <p>
  * Слова следующие:
  * <p>
@@ -32,12 +32,75 @@ package ru.ifmo.cet.javabasics;
  */
 public class BottleSong {
 
+    private int delta;
+    private StringBuilder resultSong = new StringBuilder();
+
     public BottleSong(int bottleTakenAtOnce) {
-        //TODO
+        delta = bottleTakenAtOnce;
     }
 
     public String getBottleSongLyrics() {
-        //TODO
-        throw new UnsupportedOperationException();
+        return Generate(99) + "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.";
+    }
+
+    StringBuilder tempBuilder = new StringBuilder();
+    private String Generate(int temp){
+        tempBuilder.setLength(0);
+        tempBuilder.append(temp).append(" ").append(PluralCheck("bottle", "bottles", temp)).append(" of beer on the wall, ").append(PluralCheck("bottle", "bottles", temp)).append(" of beer.\nTake ");
+
+        temp -= delta;
+
+        if (temp > 0){
+            tempBuilder.append(NumberToWord(delta)).append(" down and pass around, ").append(temp).append(" ").append(PluralCheck("bottle", "bottles", temp)).append(" of beer on the wall.\n");
+            resultSong.append(tempBuilder);
+            return Generate(temp);
+        }
+
+        if (temp == 0){
+            tempBuilder.append(NumberToWord(delta));
+        }else{
+            tempBuilder.append(NumberToWord(temp+delta));
+        }
+
+        tempBuilder.append(" down and pass around, no more bottles of beer on the wall.\n");
+
+        resultSong.append(tempBuilder);
+
+        return resultSong.toString();
+    }
+
+    private static final String[] zeroToNine = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    private static final String[] elevenToNineteen = {"eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"};
+    private static final String[] tenToNinety = {"ten","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"};
+
+    private String PluralCheck(String baseValue, String pluralValue, int ammount){
+        if (ammount > 1){
+            return pluralValue;
+        }
+
+        return baseValue;
+    }
+
+    private String NumberToWord(int number){
+        if (number <= 0){
+            return "";
+        }
+
+        if (number < 10){
+            return zeroToNine[number-1];
+        }
+
+        if (number < 20){
+            return elevenToNineteen[number-1];
+        }
+
+        int decimalPart = (number / 10);
+        int anotherPart = number % 10;
+
+        if (anotherPart == 0){
+            return tenToNinety[decimalPart-1];
+        }
+
+        return tenToNinety[decimalPart-1] + " " + zeroToNine[anotherPart-1];
     }
 }
--- src/test/java/ru/ifmo/cet/javabasics/BottleSongTest.java
+++ src/test/java/ru/ifmo/cet/javabasics/BottleSongTest.java
@@ -1,4 +1,4 @@
-package ru.ifmo.cet.javabasics;
+    package ru.ifmo.cet.javabasics;
 
 import org.junit.jupiter.api.Test;
 
