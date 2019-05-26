9c2ae98ceef6da94741ce57a7034284b9b32d542
marycia624 <265103@niuitmo.ru>
2019-05-26 20:28:37 +1000

    Was difficult

--- src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
+++ src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
@@ -2,20 +2,46 @@ package ru.ifmo.cet.javabasics;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+
 
 
 public class WarAndPeaceExercise {
 
-    public static String warAndPeace() {
+    public static String warAndPeace() throws IOException {
         final Path tome12Path = Paths.get("src", "main", "resources", "WAP12.txt");
         final Path tome34Path = Paths.get("src", "main", "resources", "WAP34.txt");
 
-        // TODO map lowercased words to its amount in text and concatenate its entries.
-        // TODO Iff word "котик" occurred in text 23 times then its entry would be "котик - 23\n".
-        // TODO Entries in final String should be also sorted by amount and then in alphabetical order iff needed.
-        // TODO Also omit any word with lengths less than 4 and frequency less than 10
+        StringBuilder result = new StringBuilder();
 
-        throw new UnsupportedOperationException();
+        Stream<String> summary = Stream.concat(Files.lines(tome12Path, Charset.forName("windows-1251")), Files.lines(tome34Path, Charset.forName("windows-1251")));
+
+
+        summary.map((p) -> p.toLowerCase())
+                .map((p) -> p.replaceAll("[^a-zа-я]", " "))
+                .flatMap((p) -> Arrays.stream(p.split(" ")))
+                .filter((p) -> p.length() >= 4)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet()
+                .stream()
+                .filter((p) -> p.getValue() >= 10)
+                .sorted(Comparator.<Map.Entry<String, Long>, Long>comparing((p) -> p.getValue())
+                        .reversed()
+                        .thenComparing((p) -> p.getKey()))
+                .forEach((p) -> result.append(p.getKey()).append(" - ").append(p.getValue()).append("\n"));
+
+        return result.toString().trim();
+
+
     }
 
 }
