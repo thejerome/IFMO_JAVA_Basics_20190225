691067c2b81aab9169464feaf2adcad7797ee6dd
Vikatimoshenko3103 <43112011+Vikatimoshenko3103@users.noreply.github.com>
2019-05-28 23:43:31 +0300

    Final

--- src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
+++ src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
@@ -1,39 +1,67 @@
 package ru.ifmo.cet.javabasics;
 
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.file.*;
 import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.*;
 import java.util.*;
-import java.util.function.Predicate;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.charset.Charset;
+
 
 public class WarAndPeaceExercise {
+    private static Map<String, Integer> sortByValue(Map<String, Integer> unsortMap) {
+        List<Map.Entry<String, Integer>> list = new LinkedList<>(unsortMap.entrySet());
+
+        Collections.sort(list, new Comparator<>() {
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                if (o1.getValue().equals(o2.getValue())) return o1.getKey().compareTo(o2.getKey());
+                else return -(o1.getValue()).compareTo(o2.getValue());
+            }
+        });
+
+        Map<String, Integer> sortedMap = new LinkedHashMap<>();
+        for (Map.Entry<String, Integer> entry : list) {
+            sortedMap.put(entry.getKey(), entry.getValue());
+        }
+
+        return sortedMap;
+    }
 
     public static String warAndPeace() throws IOException {
-
         final Path tome12Path = Paths.get("src", "main", "resources", "WAP12.txt");
         final Path tome34Path = Paths.get("src", "main", "resources", "WAP34.txt");
-        List<String> listNovel = Files.readAllLines(tome12Path, Charset.forName("windows-1251"));
-        listNovel.addAll(Files.readAllLines(tome34Path, Charset.forName("windows-1251")));
 
-        Map<String, Integer> words = new HashMap<>();
-        for (String Line : listNovel)
-            for (String word : Line.split("[\\s,.[0-9]«»()?!\\]\\[;:'\\“\"…„]+"))
-                if ((word = word.toLowerCase()).length() >= 4)
-                    words.put(word, 1 + (words.keySet().contains(word) ? words.get(word) : 0));
+        List<String> parseBase = Files.readAllLines(tome12Path, Charset.forName("windows-1251"));
+        parseBase.addAll(Files.readAllLines(tome34Path, Charset.forName("windows-1251")));
 
-        List<Map.Entry<String, Integer>> wordFreq = new ArrayList<>(words.entrySet());
-        wordFreq.sort(Map.Entry.comparingByKey());
-        wordFreq.sort(Map.Entry.comparingByValue(new Comparator<Integer>() {
-            @Override public int compare(Integer x, Integer y) { return y - x; }}));
-        wordFreq.removeIf(new Predicate<Map.Entry<String, Integer>>() {
-            @Override public boolean test(Map.Entry<String, Integer> x) { return x.getValue() < 10; }});
+        Map<String, Integer> data = new HashMap<>();
 
-        StringBuilder result = new StringBuilder();
-        for (Map.Entry<String, Integer> p : wordFreq)
-            result.append(p.getKey() + " - " + p.getValue() + "\n");
+        for (var tempLine : parseBase) {
+            String temp = tempLine.toLowerCase().replaceAll("[^а-яa-z]", " ");
 
-        return result.substring(0, result.length() - 1);
+            for (var tempWord : temp.split(" ")) {
+                if (tempWord.length() > 3) {
+                    if (!data.containsKey(tempWord)) {
+                        data.put(tempWord, 0);
+                    }
+                    data.put(tempWord, data.get(tempWord) + 1);
+                }
+            }
+        }
+
+        Map<String, Integer> srtdata = sortByValue(data);
+
+        String generated = "";
+
+        for (Map.Entry<String, Integer> entry : srtdata.entrySet()) {
+            String key = entry.getKey();
+            Integer value = entry.getValue();
+            if (value >= 10) {
+                generated += key + " - " + value + "\n";
+            }
+        }
+
+        return generated.substring(0, generated.length() - 1);
+
     }
 }
