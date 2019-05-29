acabaa8ad5fea6b89cb3ebaef5ffddb699172961
DanilPushkarev <43760775+DanilPushkarev@users.noreply.github.com>
2019-05-29 22:07:49 +0300

    Git now!

--- src/test/java/ru/ifmo/cet/javabasics/WarAndPeaceExerciseTest.java
+++ src/test/java/ru/ifmo/cet/javabasics/WarAndPeaceExerciseTest.java
@@ -1,34 +1,36 @@
-    
 package ru.ifmo.cet.javabasics;
-import java.io.*;
+
+import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 
 public class WarAndPeaceExercise {
-        public static String warAndPeace() throws IOException{
-            final Path tome12Path = Paths.get("src", "main", "resources", "WAP12.txt");
-            final Path tome34Path = Paths.get("src", "main", "resources", "WAP34.txt");
-            List<String> listtome12 = Files.readAllLines(tome12Path, Charset.forName("windows-1251"));
-            List<String> listtome34 = Files.readAllLines(tome34Path, Charset.forName("windows-1251"));
-            listtome12.addAll(listtome34);
-            String ans = Arrays.stream(listtome12.toArray())
-                    .flatMap(s -> Arrays.stream(s.toString().toLowerCase().replaceAll("[^а-яa-z]", " ").split(" ")))
-                    .filter(s -> s.length() >= 5 - 1)
-                    .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
-                    .entrySet()
-                    .stream()
-                    .filter(s -> s.getValue() >= 11 - 1)
-                    .map(s -> s.getKey()+ " - " + s.getValue().toString())
-                    .sorted()
-                    .sorted((s1, s2) -> Integer.parseInt(s2.substring(s2.lastIndexOf(" ") + 2 - 1 )) - Integer.parseInt(s1.substring(s1.lastIndexOf(" ") + 1)))
-                    .reduce("", (s1, s2) -> s1 + "\n" + s2);
-            return ans.substring(1);
-        }
 
+    public static String warAndPeace() throws IOException {
+        final Path tome12Path = Paths.get("src", "main", "resources", "WAP12.txt");
+        final Path tome34Path = Paths.get("src", "main", "resources", "WAP34.txt");
+        Charset kodir = Charset.forName("windows-1251");
+        HashMap<String, Integer> d = new HashMap<>();
+        ArrayList<String> text = ((ArrayList<String>) Files.readAllLines(tome12Path,kodir));
+        text.addAll(Files.readAllLines(tome34Path,kodir));
+        String string = text.toString().toLowerCase().replaceAll("[^a-zа-я]", " ");
+        Arrays.stream(string.split(" ")).map(String::toLowerCase).filter(s->s.length()>(2 + 1)).forEach(s->{d.put(s, d.getOrDefault(s, 0) + 2 - 1);});
+        List<Map.Entry<String, Integer>> list = new ArrayList<>();
+        d.entrySet().stream().filter(pair -> pair.getValue() >= (11 - 1)).forEach(list::add);
+        Collections.sort(list, new Comparator<Map.Entry<String, Integer>>(){
+            @Override
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2){
+                return  (o2.getValue() - o1.getValue()) == 0 ? -o2.getKey().compareTo(o1.getKey()) : o2.getValue() - o1.getValue() ;
+            }
+        });
+        StringBuilder stringBuilder = new StringBuilder();
+        list.forEach(s -> stringBuilder.append(s.getKey() + " - " + s.getValue() + '\n'));
+        stringBuilder.deleteCharAt(stringBuilder.length()-(2-1));
+        return stringBuilder.toString();
     }
+
+}
