66d678bc022c7f6818a3bb1e5e5c2ae5322744c1
Vrezh <vrezhdevelop@mail.ru>
2019-05-29 22:54:35 +0300

    Solved

--- src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
+++ src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
@@ -1,100 +1,63 @@
 package ru.ifmo.cet.javabasics;
 
-import java.nio.file.Files;
-import java.io.*;
-import java.util.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.io.*;
 import java.nio.charset.Charset;
+import java.util.stream.Collectors;
+import java.util.*;
+import java.util.stream.Stream;
 
 
 
 public class WarAndPeaceExercise {
 
-    private static <TK,TV> String convertMapToString(Map<TK, TV> map){
-        String temp = "";
-        for (TK key : map.keySet()) {
-            temp += key + " - " + map.get(key) + "\n";
-        }
-        return temp.substring(0, temp.length()-1);
+    static List<String> finalValue = new LinkedList<String>();
+    static Map<String, Integer> wordsCount = new HashMap<String, Integer>();
+
+    public static Integer getValue(String baseValue){
+        return Integer.parseInt(baseValue.split(" - ")[1]);
     }
 
-    private static boolean filterCheck(String filter, Integer value, Integer target){
-        if (filter.contains("=") && value.equals(target)){
-            return true;
-        }
-
-        if (filter.contains(">") && value > target){
-            return true;
-        }
-
-        if (filter.contains("<") && value < target){
-            return  true;
-        }
-
-        return false;
+    public static String getKey(String baseValue){
+        return baseValue.split(" - ")[0];
     }
 
-    private static Map<String, Integer> mapFilterIntParam (Map<String, Integer> map, String filter, Integer targetValue){
-        Map<String, Integer> temp = new HashMap<String,Integer>();
-        for (Map.Entry<String, Integer> entry : map.entrySet()) {
-            String key = entry.getKey();
-            Integer value = entry.getValue();
-            if (filterCheck(filter, value, targetValue)){
-                temp.put(key, value);
-            }
-        }
-
-        return  temp;
+    public static void addWord(String word){
+        wordsCount.put(word, wordsCount.getOrDefault(word, 0) + 1);
+        finalValue.add(word);
     }
 
-    private static Map<String, Integer> sortMap(Map<String, Integer> unsortMap) {
-        List<Map.Entry<String, Integer>> list = new LinkedList<Map.Entry<String, Integer>>(unsortMap.entrySet());
-
-        Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
-            public int compare(Map.Entry<String,Integer> a, Map.Entry<String,Integer> b){
-                if(a.getValue().equals(b.getValue())) {
-                    return a.getKey().compareTo(b.getKey());
-                }
-
-                return -(a.getValue()).compareTo(b.getValue());
-            }
-        });
-
-        Map<String, Integer> sortedMap = new LinkedHashMap<String, Integer>();
-        for (Map.Entry<String, Integer> entry : list) {
-            sortedMap.put(entry.getKey(), entry.getValue());
-        }
-
-        return sortedMap;
-    }
-
-    public static String warAndPeace() throws IOException  {
-        // TODO map lowercased words to its amount in text and concatenate its entries.
-        // TODO Iff word "котик" occurred in text 23 times then its entry would be "котик - 23\n".
-        // TODO Entries in final String should be also sorted by amount and then in alphabetical order iff needed.
-        // TODO Also omit any word with lengths less than 4 and frequency less than 10
-
+    public static String warAndPeace() throws IOException {
         final Path tome12Path = Paths.get("src", "main", "resources", "WAP12.txt");
         final Path tome34Path = Paths.get("src", "main", "resources", "WAP34.txt");
 
-        List<String> readenData = Files.readAllLines(tome12Path, Charset.forName("windows-1251"));
-        readenData.addAll(Files.readAllLines(tome34Path, Charset.forName("windows-1251")));
+        Stream<String> streamFromFiles = Stream.concat(Files.lines(tome12Path.toAbsolutePath(), Charset.forName("windows-1251")), Files.lines(tome34Path.toAbsolutePath(), Charset.forName("windows-1251")));
 
-        Map<String, Integer> finalList = new HashMap<String,Integer>();
+        streamFromFiles
+                .forEach((l) -> Stream.of(l.toLowerCase().replaceAll("[^а-яa-z]", " ").split(" ")).filter(r -> r.length() >= 4).forEach(
+                        (w) -> addWord(w))
+                );
 
-        for (String line : readenData){
-            for (String word : line.toLowerCase().replaceAll("[^а-яa-z]", " ").split(" ")){
-                if (word.length() > 3){
-                    if (finalList.containsKey(word)){
-                        finalList.put(word, finalList.get(word)+1);
-                    }else{
-                        finalList.put(word, 1);
-                    }
-                }
+
+        StringBuilder generator = new StringBuilder();
+
+        Comparator<String> comparator = new Comparator<String>() {
+            @Override
+            public int compare(String i1, String i2) {
+                int valueComaration = getValue(i2).compareTo(getValue(i1));
+                return valueComaration == 0 ?  getKey(i1).compareTo(getKey(i2)): valueComaration;
             }
-        }
+        };
 
-        return convertMapToString(sortMap(mapFilterIntParam(finalList, ">=", 10)));
+        finalValue.stream()
+                .map(s -> s + " - " + wordsCount.get(s))
+                .distinct()
+                .sorted(comparator)
+                .filter(t -> getValue(t) >= 10)
+                .forEach(word -> generator.append(word + "\n"));
+
+        return  generator.toString().substring(0, generator.toString().length()-1);
     }
 }
