fddd98dd8e75d6adef002f6eca4aef4ef9a31495
DanilPushkarev <43760775+DanilPushkarev@users.noreply.github.com>
2019-05-29 21:58:45 +0300

    Git happens!

--- src/test/java/ru/ifmo/cet/javabasics/WarAndPeaceExerciseTest.java
+++ src/test/java/ru/ifmo/cet/javabasics/WarAndPeaceExerciseTest.java
@@ -1,42 +1,34 @@
+    
 package ru.ifmo.cet.javabasics;
-
-import org.junit.jupiter.api.Test;
-
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
-import static java.nio.file.Files.readAllLines;
-import static org.junit.jupiter.api.Assertions.*;
 
-/**
- * Created by EE on 2018-09-20.
- */
-class WarAndPeaceExerciseTest {
-
-    @Test
-    void warAndPeace() throws IOException {
-        final String result = WarAndPeaceExercise.warAndPeace();
-        assertEquals(new WAPResult().result, result);
-    }
-
-    @Test
-    void testLambdas() throws IOException {
-        final String source = readAllLines(Paths.get(
-            "src", "main", "java", "ru", "ifmo", "cet", "javabasics", "WarAndPeaceExercise.java"
-        )).stream().collect(Collectors.joining("\n"));
-
-        assertFalse(matchesRegex(source, "\\Wif\\W"));
-        assertFalse(matchesRegex(source, "\\Wfor\\W"));
-        assertFalse(matchesRegex(source, "\\Wwhile\\W"));
+public class WarAndPeaceExercise {
+        public static String warAndPeace() throws IOException{
+            final Path tome12Path = Paths.get("src", "main", "resources", "WAP12.txt");
+            final Path tome34Path = Paths.get("src", "main", "resources", "WAP34.txt");
+            List<String> listtome12 = Files.readAllLines(tome12Path, Charset.forName("windows-1251"));
+            List<String> listtome34 = Files.readAllLines(tome34Path, Charset.forName("windows-1251"));
+            listtome12.addAll(listtome34);
+            String ans = Arrays.stream(listtome12.toArray())
+                    .flatMap(s -> Arrays.stream(s.toString().toLowerCase().replaceAll("[^а-яa-z]", " ").split(" ")))
+                    .filter(s -> s.length() >= 5 - 1)
+                    .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                    .entrySet()
+                    .stream()
+                    .filter((s -> s.getValue() >= 11 - 1) or (s -> s.getValue() >= 9 + 1) )
+                    .map(s -> s.getKey()+ " - " + s.getValue().toString())
+                    .sorted()
+                    .sorted((s1, s2) -> Integer.parseInt(s2.substring(s2.lastIndexOf(" ") + 2 - 1 )) - Integer.parseInt(s1.substring(s1.lastIndexOf(" ") + 1)))
+                    .reduce("", (s1, s2) -> s1 + "\n" + s2);
+            return ans.substring(1);
+        }
 
     }
-
-    private boolean matchesRegex(String source, String regex) {
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(source);
-        return (matcher.find());
-    }
-}
