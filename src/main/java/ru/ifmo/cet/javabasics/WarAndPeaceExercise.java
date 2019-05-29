9909e3dd79856cca2005b0506be40bd1c9b426af
Budurgus <burdaev-igor@mail.ru>
2019-05-30 00:21:26 +0300

    Task-2

--- src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
+++ src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
@@ -1,21 +1,32 @@
 package ru.ifmo.cet.javabasics;
 
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.util.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 public class WarAndPeaceExercise {
-
-    public static String warAndPeace() {
+    public static String warAndPeace() throws IOException {
         final Path tome12Path = Paths.get("src", "main", "resources", "WAP12.txt");
         final Path tome34Path = Paths.get("src", "main", "resources", "WAP34.txt");
+        Stream<String> Text = Stream.concat(Files.lines(tome12Path.toAbsolutePath(), Charset.forName("windows-1251")), Files.lines(tome34Path.toAbsolutePath(), Charset.forName("windows-1251")));
 
-        // TODO map lowercased words to its amount in text and concatenate its entries.
-        // TODO Iff word "котик" occurred in text 23 times then its entry would be "котик - 23\n".
-        // TODO Entries in final String should be also sorted by amount and then in alphabetical order iff needed.
-        // TODO Also omit any word with lengths less than 4 and frequency less than 10
-
-        throw new UnsupportedOperationException();
+        String Note = Text.map(String::toLowerCase).map(s -> s.replaceAll("[^а-яa-z]", " "))
+                .map(s -> s.split(" ")).flatMap(s -> Arrays.stream(s))
+                .filter((s) -> s.length() >= 4).collect(Collectors.groupingBy(s -> s, Collectors.counting()))
+                .entrySet().stream().filter(s -> s.getValue() >= 10).sorted(new Comparator<Map.Entry<String, Long>>() {
+                    @Override
+                    public int compare(Map.Entry<String, Long> a, Map.Entry<String, Long> b) {
+                        return a.getValue().compareTo(b.getValue()) == 0
+                                ? a.getKey().compareTo(b.getKey())
+                                : a.getValue().compareTo(b.getValue()) * (-1);
+                    }
+                }).map(s -> s.getKey() + " - " + s.getValue()).reduce("", (x, y) -> x + y + "\n").trim();
+        return Note;
     }
-
 }
