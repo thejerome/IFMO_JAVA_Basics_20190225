8470ddd819cee9ed7bd6c4310451151c9e0b1f99
DanilPushkarev <43760775+DanilPushkarev@users.noreply.github.com>
2019-05-29 23:24:04 +0300

    Update WarAndPeaceExerciseTest.java

--- src/test/java/ru/ifmo/cet/javabasics/WarAndPeaceExerciseTest.java
+++ src/test/java/ru/ifmo/cet/javabasics/WarAndPeaceExerciseTest.java
@@ -1,36 +1,42 @@
 package ru.ifmo.cet.javabasics;
 
+import org.junit.jupiter.api.Test;
+
 import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
+import static java.nio.file.Files.readAllLines;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class WarAndPeaceExercise {
+/**
+ * Created by EE on 2018-09-20.
+ */
+class WarAndPeaceExerciseTest {
 
-    public static String warAndPeace() throws IOException {
-        final Path tome12Path = Paths.get("src", "main", "resources", "WAP12.txt");
-        final Path tome34Path = Paths.get("src", "main", "resources", "WAP34.txt");
-        Charset kodir = Charset.forName("windows-1251");
-        HashMap<String, Integer> d = new HashMap<>();
-        ArrayList<String> text = ((ArrayList<String>) Files.readAllLines(tome12Path,kodir));
-        text.addAll(Files.readAllLines(tome34Path,kodir));
-        String string = text.toString().toLowerCase().replaceAll("[^a-zа-я]", " ");
-        Arrays.stream(string.split(" ")).map(String::toLowerCase).filter(s->s.length()>(2 + 1)).forEach(s->{d.put(s, d.getOrDefault(s, 0) + 2 - 1);});
-        List<Map.Entry<String, Integer>> list = new ArrayList<>();
-        d.entrySet().stream().filter(pair -> pair.getValue() >= (11 - 1)).forEach(list::add);
-        Collections.sort(list, new Comparator<Map.Entry<String, Integer>>(){
-            @Override
-            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2){
-                return  (o2.getValue() - o1.getValue()) == 0 ? -o2.getKey().compareTo(o1.getKey()) : o2.getValue() - o1.getValue() ;
-            }
-        });
-        StringBuilder stringBuilder = new StringBuilder();
-        list.forEach(s -> stringBuilder.append(s.getKey() + " - " + s.getValue() + '\n'));
-        stringBuilder.deleteCharAt(stringBuilder.length()-(2-1));
-        return stringBuilder.toString();
+    @Test
+    void warAndPeace() throws IOException {
+        final String result = WarAndPeaceExercise.warAndPeace();
+        assertEquals(new WAPResult().result, result);
     }
 
+    @Test
+    void testLambdas() throws IOException {
+        final String source = readAllLines(Paths.get(
+            "src", "main", "java", "ru", "ifmo", "cet", "javabasics", "WarAndPeaceExercise.java"
+        )).stream().collect(Collectors.joining("\n"));
+
+        assertFalse(matchesRegex(source, "\\Wif\\W"));
+        assertFalse(matchesRegex(source, "\\Wfor\\W"));
+        assertFalse(matchesRegex(source, "\\Wwhile\\W"));
+
+    }
+
+    private boolean matchesRegex(String source, String regex) {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(source);
+        return (matcher.find());
+    }
 }
