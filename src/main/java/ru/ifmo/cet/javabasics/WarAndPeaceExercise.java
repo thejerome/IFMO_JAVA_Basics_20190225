c1016a0d22b9074e27db07479b5ae611694c669c
Игорь Каленик <kalenik.igor@gmail.com>
2019-06-01 02:07:03 +0300

    solved_task_3

--- src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
+++ src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
@@ -1,21 +1,39 @@
 package ru.ifmo.cet.javabasics;
 
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
-public class WarAndPeaceExercise {
+public static String warAndPeace() throws IOException {
 
     public static String warAndPeace() {
         final Path tome12Path = Paths.get("src", "main", "resources", "WAP12.txt");
         final Path tome34Path = Paths.get("src", "main", "resources", "WAP34.txt");
 
-        // TODO map lowercased words to its amount in text and concatenate its entries.
-        // TODO Iff word "котик" occurred in text 23 times then its entry would be "котик - 23\n".
-        // TODO Entries in final String should be also sorted by amount and then in alphabetical order iff needed.
-        // TODO Also omit any word with lengths less than 4 and frequency less than 10
+        Stream<String> txtLines12 = Files.lines(tome12Path.toAbsolutePath(), Charset.forName("windows-1251"));
+        Stream<String> txtLines34 = Files.lines(tome34Path.toAbsolutePath(), Charset.forName("windows-1251"));
+        Stream<String> txtLines = Stream.concat(txtLines12,txtLines34);
 
-        throw new UnsupportedOperationException();
+        StringBuilder result = new StringBuilder();
+
+        txtLines.map(String::toLowerCase).map(lines -> lines.replaceAll("[^a-zа-я]"," "))
+        .flatMap(lines -> Arrays.stream(lines.split(" ")))
+        .filter(word -> word.length()>3)
+        .collect(Collectors.groupingBy(Function.identity(),Collectors.counting()))
+        .entrySet()
+        .stream()
+        .filter(word -> word.getValue()>=10)
+        .sorted((o1, o2) -> o1.getValue().equals(o2.getValue()) ? o1.getKey().compareTo(o2.getKey()) : -(o1.getValue()).compareTo(o2.getValue()))
+        .forEach(word -> result.append(word.getKey()).append(" - ").append(word.getValue()).append("\n"));
+
+        return result.toString().trim();
     }
 
 }
