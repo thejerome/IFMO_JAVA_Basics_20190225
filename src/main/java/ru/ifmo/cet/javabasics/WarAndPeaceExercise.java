3bad42ac3c1ef73aa47624dbf98dc71c3448fd82
Игорь Каленик <kalenik.igor@gmail.com>
2019-06-01 02:14:43 +0300

    solved_task_3

--- src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
+++ src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
@@ -10,10 +10,9 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+public class WarAndPeaceExercise {
 
-public static String warAndPeace() throws IOException {
-
-    public static String warAndPeace() {
+    public static String warAndPeace() throws IOException {
         final Path tome12Path = Paths.get("src", "main", "resources", "WAP12.txt");
         final Path tome34Path = Paths.get("src", "main", "resources", "WAP34.txt");
 
@@ -24,16 +23,15 @@ public static String warAndPeace() throws IOException {
         StringBuilder result = new StringBuilder();
 
         txtLines.map(String::toLowerCase).map(lines -> lines.replaceAll("[^a-zа-я]"," "))
-        .flatMap(lines -> Arrays.stream(lines.split(" ")))
-        .filter(word -> word.length()>3)
-        .collect(Collectors.groupingBy(Function.identity(),Collectors.counting()))
-        .entrySet()
-        .stream()
-        .filter(word -> word.getValue()>=10)
-        .sorted((o1, o2) -> o1.getValue().equals(o2.getValue()) ? o1.getKey().compareTo(o2.getKey()) : -(o1.getValue()).compareTo(o2.getValue()))
-        .forEach(word -> result.append(word.getKey()).append(" - ").append(word.getValue()).append("\n"));
+                .flatMap(lines -> Arrays.stream(lines.split(" ")))
+                .filter(word -> word.length()>3)
+                .collect(Collectors.groupingBy(Function.identity(),Collectors.counting()))
+                .entrySet()
+                .stream()
+                .filter(word -> word.getValue()>=10)
+                .sorted((o1, o2) -> o1.getValue().equals(o2.getValue()) ? o1.getKey().compareTo(o2.getKey()) : -(o1.getValue()).compareTo(o2.getValue()))
+                .forEach(word -> result.append(word.getKey()).append(" - ").append(word.getValue()).append("\n"));
 
         return result.toString().trim();
     }
-
 }
