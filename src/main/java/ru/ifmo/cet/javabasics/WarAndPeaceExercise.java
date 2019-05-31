f20d1c9a58c1bd90dfb9db02dc0170a9c7d4b0ef
Streaxz <42956553+Streaxz@users.noreply.github.com>
2019-05-31 20:43:35 +0300

    haf

--- src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
+++ src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
@@ -1,41 +1,75 @@
 package ru.ifmo.cet.javabasics;
-	
-	
-	import java.io.*;
-	import java.nio.charset.Charset;
-	import java.nio.file.Files;
-	import java.util.*;
-	import java.nio.file.Path;
-	import java.nio.file.Paths;
-	import java.util.stream.Collectors;
-	
-	
-	
-	
-	public class WarAndPeaceExercise {
-	
-	
-	    public static String warAndPeace() throws IOException{
-	        final Path tome12Path = Paths.get("src", "main", "resources", "WAP12.txt");
-	        final Path tome34Path = Paths.get("src", "main", "resources", "WAP34.txt");
-	        List<String> volumes12 = Files.readAllLines(tome12Path.toAbsolutePath(), Charset.forName("windows-1251"));
-	        List<String> volumes34 = Files.readAllLines(tome34Path.toAbsolutePath(), Charset.forName("windows-1251"));
-	        volumes12.addAll(volumes34);
-	
-	
-	
-	
-	        String frequences = volumes12.stream().map(String :: toLowerCase).map(s -> s.replaceAll("[^а-яa-z]", " "))
-	                .map(s -> s.split(" ")).flatMap(s -> Arrays.stream(s))
-	                .filter((s) -> s.length() >= 4).collect(Collectors.groupingBy(s -> s, Collectors.counting()))
-	                .entrySet().stream().filter(s -> s.getValue() >= 10)
-	                .sorted(new Comparator<Map.Entry<String, Long>>() {
-	                    @Override
-	                    public int compare(Map.Entry<String, Long> o1, Map.Entry<String, Long> o2) {
-	                        return o1.getValue().compareTo(o2.getValue()) == 0
-	                                ? o1.getKey().compareTo(o2.getKey())
-	                                : o1.getValue().compareTo(o2.getValue()) * (-1);
-	                    }
-	                }).map(s -> s.getKey() + " - " +s.getValue()).reduce("", (x, y) -> x + y + "\n").trim();
-	        return frequences;
-	    }
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+
+import static java.nio.file.Files.readAllLines;
+
+
+public class WarAndPeaceExercise {
+
+    public static String warAndPeace() throws IOException
+    {
+        final Path tome12Path = Paths.get("src", "main", "resources", "WAP12.txt");
+        final Path tome34Path = Paths.get("src", "main", "resources", "WAP34.txt");
+
+        Map<String, Integer> mp = new HashMap<>();
+        List<List<String>> WaPList = new ArrayList<>();
+
+        Integer[] FuckCodeQuality = new Integer[1];
+
+        WaPList.add(readAllLines(tome12Path, Charset.forName("windows-1251")));
+        WaPList.add(readAllLines(tome34Path, Charset.forName("windows-1251")));
+        WaPList.forEach(tomes -> tomes.forEach(lines -> Arrays.stream(lines.split("[\\s,.[0-9]«»()?!\\]\\[;:'\\“\"…„]+")).filter(x -> x.length() >= 4).map(String::toLowerCase).forEach(word -> {
+            FuckCodeQuality[0] = (mp.keySet().contains(word)) ? mp.put(word, mp.get(word) + 1) : mp.put(word, 1);
+        })));
+        FuckCodeQuality[0]++;
+        List<Map.Entry<String, Integer>> list = new ArrayList<>(mp.entrySet());
+        SortedSet<KeyValuePair> sortedSet = new TreeSet<KeyValuePair>();
+        list.stream().filter(sort -> sort.getValue() >= 10).forEach(item -> {
+            sortedSet.add(new KeyValuePair(item.getValue(),item.getKey()));
+        });
+        StringBuilder builder = new StringBuilder();
+        sortedSet.forEach(item -> {
+            builder.append(item.toString());
+        });
+        return builder.substring(0, builder.length()-1);
+    }
+
+}
+
+class KeyValuePair implements Comparable<KeyValuePair> {
+    private String key;
+    private long value;
+
+    KeyValuePair(long value, String key) {
+        super();
+        this.key = key;
+        this.value = value;
+    }
+
+    public long getValue() {
+        return value;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setValue(long value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return key + " - " + value + "\n";
+    }
+
+    @Override
+    public int compareTo(KeyValuePair o) {
+        return value != o.value ? (int) (o.value - value) : key.compareTo(o.key);
+    }
+}
